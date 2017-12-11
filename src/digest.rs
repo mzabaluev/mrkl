8dfdb11ddfc9c32162221147830b18bd1225b5f2
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2017-12-11 22:45:37 +0200

    Make DigestHasher work with any EndianInput impl
    This gets rid of the byte order type parameter.
    Yay composability!

--- src/digest.rs
+++ src/digest.rs
@@ -12,8 +12,7 @@ use tree::{Nodes, Node};
 pub extern crate digest_hash;
 
 use self::digest_hash::{Hash, Endian, EndianInput};
-use self::digest_hash::byteorder::ByteOrder;
-use self::digest_hash::digest::{Input, FixedOutput};
+use self::digest_hash::digest::FixedOutput;
 use self::digest_hash::digest::generic_array::GenericArray;
 
 use std::fmt;
@@ -22,40 +21,38 @@ use std::marker::PhantomData;
 
 
 #[derive(Clone)]
-pub struct DigestHasher<In: ?Sized, D, Bo> {
-    phantom: PhantomData<(*const In, Endian<D, Bo>)>
+pub struct DigestHasher<In: ?Sized, D> {
+    phantom: PhantomData<(*const In, D)>
 }
 
-impl<In: ?Sized, D, Bo> Default for DigestHasher<In, D, Bo>
-    where D: EndianInput,
-          Bo: ByteOrder
+impl<In: ?Sized, D> Default for DigestHasher<In, D>
+    where D: EndianInput
 {
     fn default() -> Self {
         DigestHasher { phantom: PhantomData }
     }
 }
 
-impl<In: ?Sized, D, Bo> Debug for DigestHasher<In, D, Bo>
-    where Bo: ByteOrder
+impl<In: ?Sized, D> Debug for DigestHasher<In, D>
+    where D: EndianInput
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         f.debug_tuple("DigestHasher")
-         .field(&Endian::<D, Bo>::byte_order_str())
+         .field(&Endian::<D, D::ByteOrder>::byte_order_str())
          .finish()
     }
 }
 
-impl<In: ?Sized, D, Bo> Hasher<In> for DigestHasher<In, D, Bo>
+impl<In: ?Sized, D> Hasher<In> for DigestHasher<In, D>
     where In: Hash,
           D: Default,
-          D: Input,
-          D: FixedOutput,
-          Bo: ByteOrder
+          D: EndianInput,
+          D: FixedOutput
 {
     type HashOutput = GenericArray<u8, D::OutputSize>;
 
     fn hash_input(&self, input: &In) -> Self::HashOutput {
-        let mut digest = Endian::<D, Bo>::default();
+        let mut digest = D::default();
         input.hash(&mut digest);
         digest.fixed_result()
     }
@@ -90,13 +87,12 @@ mod tests {
 
     use self::sha2::{Sha256, Digest};
     use super::digest_hash::BigEndian;
-    use super::digest_hash::byteorder;
 
     const TEST_DATA: &'static [u8] = b"The quick brown fox jumps over the lazy dog";
 
     #[test]
     fn hasher_hash_input() {
-        let hasher = DigestHasher::<&'static [u8], BigEndian<Sha256>, byteorder::BigEndian>::default();
+        let hasher = DigestHasher::<&'static [u8], BigEndian<Sha256>>::default();
         let hash = hasher.hash_input(&TEST_DATA);
         assert_eq!(hash, Sha256::digest(TEST_DATA));
     }
