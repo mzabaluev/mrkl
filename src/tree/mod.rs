ab6dd86010fbc33bd7bdb6e5362aaf1e09d96678
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2017-12-13 13:28:29 +0200

    Document mod tree

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -6,29 +6,52 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! Data types for representation and construction of Merkle trees.
+
 mod builder;
 pub use self::builder::{Builder, EmptyTree};
 
 use std::iter::{Iterator, DoubleEndedIterator, ExactSizeIterator};
 use std::slice;
 
+/// A complete Merkle tree.
+///
+/// Values of this type represent complete Merkle trees.
+/// A valid tree either has a single leaf node as the root node,
+/// or has a hierarchy of nodes terminating with leaf nodes and
+/// with hash-only nodes at levels above leaves.
+///
+/// `MerkleTree` hierarchies are immutable: it's not possible to e.g.
+/// swap out nodes in safe code because doing so would violate
+/// the hash integrity.
 #[derive(Debug)]
 pub struct MerkleTree<H, T> {
     root: Node<H, T>
 }
 
+/// A Merkle tree node, which can be either a leaf node or a hash node.
+///
+/// `Node` values can be borrowed from under a `MerkleTree`.
 #[derive(Debug)]
 pub enum Node<H, T> {
+    /// A leaf node value.
     Leaf(LeafNode<H, T>),
+    /// A hash node value with child nodes.
     Hash(HashNode<H, T>)
 }
 
+/// A value representing a leaf node.
+///
+/// `LeafNode` values can be obtained by destructuring `Node`.
 #[derive(Debug)]
 pub struct LeafNode<H, T> {
     hash: H,
     data: T
 }
 
+/// A value representing a node containing the hash of its child nodes.
+///
+/// `HashNode` values can be obtained by destructuring `Node`.
 #[derive(Debug)]
 pub struct HashNode<H, T> {
     hash: H,
@@ -36,10 +59,12 @@ pub struct HashNode<H, T> {
 }
 
 impl<H, T> MerkleTree<H, T> {
+    /// Returns the root node of the tree as a borrowed reference.
     pub fn root(&self) -> &Node<H, T> { &self.root }
 }
 
 impl<H, T> Node<H, T> {
+    /// Returns a reference to the hash value of the tree node.
     pub fn hash(&self) -> &H {
         match *self {
             Node::Leaf(ref ln) => &ln.hash,
@@ -49,37 +74,49 @@ impl<H, T> Node<H, T> {
 }
 
 impl<H: AsRef<[u8]>, T> Node<H, T> {
+    /// Returns the hash value of the node as a byte slice.
     pub fn hash_bytes(&self) -> &[u8] {
         self.hash().as_ref()
     }
 }
 
 impl<H: AsRef<[u8]>, T> LeafNode<H, T> {
+    /// Returns the hash value of the leaf node as a byte slice.
     pub fn hash_bytes(&self) -> &[u8] { self.hash.as_ref() }
 }
 
 impl<H, T> LeafNode<H, T> {
+
+    /// Returns a reference to the hash value of the leaf node.
     pub fn hash(&self) -> &H { &self.hash }
+
+    /// Returns a reference to the leaf data value of the node.
     pub fn data(&self) -> &T { &self.data }
 }
 
 impl<H: AsRef<[u8]>, T> HashNode<H, T> {
+    /// Returns the hash value of the hash node as a byte slice.
     pub fn hash_bytes(&self) -> &[u8] { self.hash.as_ref() }
 }
 
 impl<H, T> HashNode<H, T> {
 
+    /// Returns a reference to the hash value of the hash node.
     pub fn hash(&self) -> &H { &self.hash }
 
+    /// Borrows a child node value at the specified index.
     pub fn child_at(&self, index: usize) -> &Node<H, T> {
         &self.children[index]
     }
 
+    /// Returns an iterator over the child nodes.
     pub fn children<'a>(&'a self) -> Nodes<'a, H, T> {
         Nodes(self.children.iter())
     }
 }
 
+/// An iterator over borrowed values of tree nodes, usually being the
+/// child nodes of a single hash node.
 #[derive(Clone, Debug)]
 pub struct Nodes<'a, H: 'a, T: 'a>(slice::Iter<'a, Node<H, T>>);
 
