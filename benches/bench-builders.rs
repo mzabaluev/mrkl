492a9b8e47e7e97dcf2a1ef7b695bd0c841c8548
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2017-12-14 23:37:27 +0200

    Added a parallel benchmark
    Using the same bench executable, to be able to compare times taken to build
    the same tree sequentially or in parallel.

---
+++ benches/bench-builders.rs
@@ -0,0 +1,86 @@
+#![feature(test)]
+
+extern crate test;
+
+extern crate mrkl;
+
+#[cfg(feature = "digest")]
+mod digest {
+    extern crate sha2;
+    extern crate digest_hash;
+
+    mod prelude {
+        pub use test::Bencher;
+        pub use test::black_box;
+
+        pub use mrkl::digest::ByteDigestHasher;
+        pub use super::sha2::Sha256;
+        pub use super::sha2::Digest;
+        pub use super::digest_hash::digest::generic_array::GenericArray;
+
+        pub type Hasher = ByteDigestHasher<Sha256>;
+    }
+
+    mod sequential {
+        use super::prelude::*;
+
+        use mrkl::tree::Builder;
+
+        use std::iter;
+
+        #[bench]
+        fn pure_digest_perf_100x4k(b: &mut Bencher) {
+            let block: &[u8] = &[0u8; 4 * 1024];
+
+            b.iter(|| {
+                let mut hash = GenericArray::default();
+                for _ in 0 .. 100 {
+                    hash = Sha256::digest(block);
+                    hash = black_box(hash);
+                }
+                for _ in 0 .. 100 - 1 {
+                    let mut digest = Sha256::new();
+                    digest.input(&[0u8][..]);
+                    digest.input(&hash);
+                    digest.input(&[0u8][..]);
+                    digest.input(&hash);
+                    hash = digest.result();
+                    hash = black_box(hash);
+                }
+            })
+        }
+
+        #[bench]
+        fn build_balanced_100x4k(b: &mut Bencher) {
+            let block: &[u8] = &[0u8; 4 * 1024];
+            let seq: Vec<_> = iter::repeat(block).take(100).collect();
+            b.iter(|| {
+                let builder = Builder::<Hasher, _>::new();
+                let tree = builder.build_balanced_from(&seq).unwrap();
+                black_box(tree);
+            })
+        }
+    }
+
+    #[cfg(feature = "parallel")]
+    mod parallel {
+        extern crate rayon;
+
+        use super::prelude::*;
+
+        use mrkl::parallel::Builder;
+
+        use self::rayon::iter;
+
+        #[bench]
+        fn build_balanced_100x4k(b: &mut Bencher) {
+            let block: &[u8] = &[0u8; 4 * 1024];
+            b.iter(|| {
+                let iter = iter::repeatn(block, 100);
+                let builder = Builder::<Hasher, _>::new();
+                let tree = builder.build_balanced_from(iter).unwrap();
+                black_box(tree);
+            })
+        }
+    }
+}
